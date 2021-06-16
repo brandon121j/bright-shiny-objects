2c819a72485e3989e084586dc7a7bc34a9291401
Brandon <brandon121j@DESKTOP-DPFP3JR.localdomain>
2021-06-16 13:54:00 -0400

    Completed addGrades function

--- main.js
+++ main.js
@@ -2,8 +2,62 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(first, last) {
+    let user = {
+    firstName: first,
+    lastName: last
+    };
+    return user;
+}
+console.log(createUser('Brandon', 'Johnson'));
 
 
+const userMe = {
+    firstName: 'Brandon',
+    lastName: 'Johnson'
+}
+
+function setAge(user, age) {
+    
+    user.age = age;
+    return user;
+}
+console.log(setAge(userMe, 50))
+
+function incrementAge(user) {
+    user.age += 1;
+    return user;
+}
+console.log(incrementAge(userMe))
+
+const car = {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 1969,
+    needsMaitenance: true
+};
+
+function fixCar(car) {
+    car.needsMaitenance = false;
+    return car;
+}
+console.log(fixCar(car))
+
+const student = {
+    name: 'Anthony DeRosa',
+    email: 'anthony.derosa@codeimmersives.com',
+    grades: [80, 100, 95]
+};
+
+const newGrades = [88, 70, 90];
+
+function addGrades(student, newGrades) {
+    for(i = 0; i < newGrades.length; i++) {
+        student.grades.push(newGrades[i])
+    }
+    return student;
+}
+console.log(addGrades(student, newGrades))
 
 
 
